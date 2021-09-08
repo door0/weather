87eba5ea5f918fc1bb852775765cb3c7d89f1fdd
door0 <moonzerochoe@gmail.com>
2021-09-08 23:15:03 +0900

    add : card

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 let latitude, longitude;
 const API_KEY = `17f3cd69b66748594cd1742315476b05`;
 
-const weather = document.querySelector(".weather");
+const card = document.querySelector(".card");
 
+// 위치 출력
 navigator.geolocation.getCurrentPosition(position => {
     latitude = position.coords.latitude; // 위도
     longitude = position.coords.longitude; // 경도
     getWeather(latitude, longitude);
 });
 
+// 날씨 출력
 function getWeather(lat, lon) {
     fetch(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&exclude=current,daily&appid=${API_KEY}&units=metric`
     ).then(response => {
         return response.json();
     }).then(json => {
@@ -20,7 +22,7 @@ function getWeather(lat, lon) {
         const min_temperature = json.main.temp_min;
         const max_temperature = json.main.temp_max;
         const place = json.name;
-        weather.innerText = `평균 기온 : ${temperature} °C 
+        card.innerText = `평균 기온 : ${temperature} °C 
         최저 기온 : ${min_temperature}°C 
         최고 기온 : ${max_temperature}°C  / @ ${place}`;
     })
