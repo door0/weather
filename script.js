b4e0896be85c4a26c0481a58297970368de88b95
door0 <68139125+door0@users.noreply.github.com>
2021-09-12 20:18:30 +0900

    update: css

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
 let latitude, longitude;
 const API_KEY = `17f3cd69b66748594cd1742315476b05`;
 
-const card = document.querySelector(".card");
+let card = document.querySelector(".card");
+let average1 = document.getElementById("average1");
+let place1 = document.getElementById("place1");
+let icon1 = document.getElementById("icon1");
+let min1 = document.getElementById("min1");
+let max1 = document.getElementById("max1");
+
+let weatherDayIcon = {
+    '01d' : 'wi-day-sunny', 
+    '02d' : 'wi-day-cloudy', 
+    '03d' : 'wi-cloud', 
+    '04d' : 'wi-cloudy', 
+    '09d' : 'wi-day-showers', 
+    '10d' : 'wi-rain', 
+    '11d' : 'wi-day-lightning', 
+    '13d' : 'wi-day-snow', 
+    '50d' : 'wi-day-fog'
+};
+
+let weatherNightIcon = {
+    '01n' : 'wi-night-clear', 
+    '02n' : 'wi-night-alt-cloudy', 
+    '03n' : 'wi-cloud', 
+    '04n' : 'wi-cloudy', 
+    '09n' : 'wi-night-alt-showers', 
+    '10n' : 'wi-rain', 
+    '11n' : 'wi-night-alt-lightning', 
+    '13n' : 'wi-night-snow', 
+    '50n' : 'wi-night-fog'
+};
 
 // 위치 출력
 navigator.geolocation.getCurrentPosition(position => {
@@ -18,12 +47,35 @@ function getWeather(lat, lon) {
         return response.json();
     }).then(json => {
         console.log(json);
-        const temperature = json.main.temp;
-        const min_temperature = json.main.temp_min;
-        const max_temperature = json.main.temp_max;
-        const place = json.name;
-        card.innerText = `평균 기온 : ${temperature} °C 
-        최저 기온 : ${min_temperature}°C 
-        최고 기온 : ${max_temperature}°C  / @ ${place}`;
+        let temperature = json.main.temp;
+        let min_temperature = json.main.temp_min;
+        let max_temperature = json.main.temp_max;
+        let place = json.name;
+        let country = json.sys.country;
+        let icon = json.weather[0].icon;
+
+        average1.innerText = `${temperature}`;
+        place1.innerText = `${place}, ${country}`;
+        min1.innerHTML = `${min_temperature}` + ` <i class="wi wi-celsius fsize20"></i>`;
+        max1.innerHTML = `${max_temperature}` + ` <i class="wi wi-celsius fsize20"></i>`;
+        // document.getElementById("icon1").src = `http://openweathermap.org/img/w/`+json.weather[0].icon+`.png`;
+
+        //낮과 밤
+        let today = new Date();
+        let hour = today.getHours();
+
+        if( hour >= 7 && hour <= 18 ) {
+            //낮
+            icon1.innerHTML = `<i class="wi `+ weatherDayIcon[icon] + `"></i>`;
+        } else {
+            icon1.innerHTML = `<i class="wi `+ weatherNightIcon[icon] + `"></i>`;
+        }
+        
+
+
+        
     })
+    
 };
+
+
